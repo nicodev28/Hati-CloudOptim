77931cbce1901d3ca9cc8428a6e07a482eafd27f
Nicolas Peysson <npeysson@hotmail.com>
2023-10-02 09:22:28 +0200

    Contruction de la structure du projet

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const fs = require('fs');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+const templateFiles = fs.readdirSync(path.resolve(__dirname, './src/'))
+  .filter((file) => path.extname(file).toLowerCase() === '.html');
+
+const htmlPluginEntries = templateFiles.map((template) => new HTMLWebpackPlugin({
+  inject: true,
+  hash: false,
+  filename: template,
+  template: path.resolve(path.resolve(__dirname, './src/'), template),
+  favicon: path.resolve(path.resolve(__dirname, './src/'), 'images', 'favicon.ico'),
+}));
+
+module.exports = {
+    entry: './src/js/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ],
+            }
+        ]
+    },
+    plugins: [
+        ...htmlPluginEntries
+    ],
+    devServer: {
+        static: path.resolve(__dirname, './dist'),
+        port: 9000
+    }
+}
